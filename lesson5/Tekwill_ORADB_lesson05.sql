2a0a6173958e8e33b3944e9ea34aec2be2fbcf05
Veronica <veronica.bagrin@ati.utm.md>
2018-05-18 20:45:16 +0300

    Lesson 5

---
+++ lesson5/Tekwill_ORADB_lesson05.sql
@@ -0,0 +1,26 @@
+--select distinct 
+--        salary, 
+  --      DEPARTMENT_ID,
+   --     EMAIL
+--from EMPLOYEES
+
+/*select first_name,
+       last_name, 
+       salary, 
+       salary*1.1, 
+       salary + COMMISSION_PCT
+from EMPLOYEES */
+
+/*select 10+15-5*2/7
+from dual */
+
+/*select first_name,
+       last_name, 
+       salary, 
+       salary*1.1,
+       COMMISSION_PCT,
+       salary + salary*COMMISSION_PCT
+from EMPLOYEES */
+
+describe employees
+
